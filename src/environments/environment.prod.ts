0f56ce5713a55d17e7faf43df67dccb266d65d21
arunlajayan <arunlajayan@gmail.com>
2021-08-18 17:12:35 +0530

    deploy angular app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,6 @@
 export const environment = {
   production: true,
-  defaultauth: 'fackbackend',
-  firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
-  },
-  apiUrl:'http://13.233.22.49:4001/api/',
+  apiURL:'https://mindcare-backend.herokuapp.com',
+  
 };
+
